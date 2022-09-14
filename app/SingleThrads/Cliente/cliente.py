862ad3fd42dbafe8cda694e1e724a563c087e604
Lucas Borguezam <lucasborguezam@gmail.com>
2022-09-13 22:21:18 -0300

    3° commit
    Identando o código

--- app/SingleThrads/Cliente/cliente.py
+++ app/SingleThrads/Cliente/cliente.py
@@ -15,39 +15,39 @@ class Cliente():
         :param port -> porta do serviço
         """
 
-        self._server_ip = server_ip
-        self._port = port
-        self._tcp = socket.socket(socket.AF.INET, socket.SOCK_STREAM)
+        self.__server_ip = server_ip
+        self.__port = port
+        self.__tcp = socket.socket(socket.AF.INET, socket.SOCK_STREAM)
 
-        def start(self):
-            """
-            Inicializa a execução do cliente
-            """
-            endpoint = (self.__server_ip, self.__port)
-            try:
-                self.__tcp.connect(endpoint)
-                print("Conexão realizada com sucesso!")
-                self.__tcp.connect(endpoint)
-                self.__method()
-            except Exception as e:
-                print("Erro na conexão com o servidor: ".e.args)
+    def start(self):
+        """
+        Inicializa a execução do cliente
+        """
+        endpoint = (self.__server_ip, self.__port)
+        try:
+            self.__tcp.connect(endpoint)
+            print("Conexão realizada com sucesso!")
+            self.__tcp.connect(endpoint)
+            self.__method()
+        except Exception as e:
+            print("Erro na conexão com o servidor: ".e.args)
 
-        def __method(self):
-            """
-            Implementa as requisições do cliente e a IHM
-            """
-            try:
-                msg = ''
-                while msg != 'x':
-                    msg = input("Digite a operação desejada (x para sair): ")
-                    if msg == '':
-                        continue  # repetir o processo
-                    elif msg == 'x':
-                        break
-                self.__tcp.send(byte(msg, 'ascii'))
-                result = self.__tcp.recv(1024)
-                print("= ", result.decode('ascii'))
+    def __method(self):
+        """
+        Implementa as requisições do cliente e a IHM
+        """
+        try:
+            msg = ''
+            while msg != 'x':
+                msg = input("Digite a operação desejada (x para sair): ")
+                if msg == '':
+                    continue  # repetir o processo
+                elif msg == 'x':
+                    break
+                self.__tcp.send(bytes(msg, 'ascii'))
+                resp = self.__tcp.recv(1024)
+                print("= ", resp.decode('ascii'))
             self.__tcp.close()
 
         except Exception as e:
-        print("Erro ao realizar a comunicação com o servidor: ".e.args)
+            print("Erro ao realizar a comunicação com o servidor: ".e.args)
--- app/SingleThrads/Servidor/servidor.py
+++ app/SingleThrads/Servidor/servidor.py
@@ -13,14 +13,16 @@ class Servidor():
         """
         Construtor da classe Servidor
         """
-        self.host = host
-        self.port = port
-
-        # OBJETO TCP,QUE VAI RECEBER AS CONFIGURAÇÕES DO SOCKET
+        self._host = host
+        self._port = port
+        self.tcp = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        # self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # OBJETO TCP,QUE RECEBE AS CONFIGURAÇÕES DO SOCKET
         # socket(tipo/classes de endereço)
         # socket_AF_INET - classe de endereços padrão
         # SOCK_STREAM - modo TCP
-        self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
 
     def start(self):
         """
@@ -30,54 +32,54 @@ class Servidor():
         try:
             self._tcp.bind(endpoint)  # anexar o servoço ao ip e porta
             self._tcp.listen(1)  # habilitar a escuta pro cliente,ou seja,estou apto a receber clientes
-            print(f"[SUCESS] Servidor iniciado em {self._host}:{self_port}")
-            while True
+            print(f"Servidor iniciado em {self._host}:{self._port}")
+            while True:
                 con, client = self._tcp.accept()  # comando bloqueante,ele fica parado ai ate o cliente se conectar
                 self._service(con, client)
         except Exception as e:
             print("Erro ao inicializar o servidor ", e.args)
 
 
-def _service(self, con, client):
-    """
-    Implementa o serviço de calculadora remota
-    :param con: objeto socket utilizado para enviar e receber os dados
-    :param client: é o endereço e porta do cliente
-    """
+    def _service(self, con, client):
+        """
+        Implementa o serviço de calculadora remota
+        :param con: objeto socket utilizado para enviar e receber os dados
+        :param client: é o endereço e porta do cliente
+        """
 
-    print("Atendendo cliente ", client)
-    operators = ['+', '-', '*', '/']
-    while True:
-        try:
-            msg = con.recv(1024)
-            msgDecoded = str(msg.decode('ascii'))  # 'op1OPERATORop2' -> '10+2'
-            op = 'none'
-            # identificar a operação
-            for x in operators:
-                if (msgDecoded.find(x) > 0:
-                op = x
-                msgDecoded = msgDecoded.split(op)  # '10+2' -> split(+) -> ['10', '2']
-                    break
-            if op == '+':
-                result = float(msgDecoded[0] + float(msgDecoded[1])
+        print("Atendendo cliente ", client)
+        operadores = ['+', '-', '*', '/']
+        while True:
+            try:
+                msg = con.recv(1024)
+                msg_s = str(msg.decode('ascii'))  # 'op1OPERATORop2' -> '10+2'
+                op = 'none'
+                # identificar a operação
+                for x in operadores:
+                    if msg.find(x) > 0:
+                        op = x
+                        msg_s = msg_s.split(op)  # '10+2' -> split(+) -> ['10', '2']
+                        break
+                if op == '+':
+                    resp = float(msg_s[0] + float(msg_s[1]))
                 elif op == '+':
-                result = float(msgDecoded[0] - float(msgDecoded[1])
+                    resp = float(msg_s[0] - float(msg_s[1]))
                 elif op == '+':
-                result = float(msgDecoded[0] * float(msgDecoded[1])
+                    resp = float(msg_s[0] * float(msg_s[1]))
                 elif op == '/':
-                result = float(msgDecoded[0] / float(msgDecoded[1])
+                    resp = float(msg_s[0] / float(msg_s[1]))
                 else:
-                result = "[ERROR] Operação Inválida"
+                    resp = "[ERROR] Operação Inválida"
 
                 # enviar a resposta
-                con.send(bytes(str(result), 'ascii'))
+                con.send(bytes(str(resp), 'ascii'))
                 print(client, " -> requisiçãoa atendida")
 
-        except OSError as e:
-            # 1° - erro de conexão
-            print("Erro de conexão ", client, ": ", e.args)
-            return  # encerra o método service
-        except Exception as e:
-            # 2° - excessão geral
-            print("Erro nos dados recebidos do cliente ->  ", client, ": ", e.args)
-            con.send(bytes('Erro', 'ascii'))
+            except OSError as e:
+                # 1° - erro de conexão
+                print("Erro de conexão ", client, ": ", e.args)
+                return  # encerra o método service
+            except Exception as e:
+                # 2° - excessão geral
+                print("Erro nos dados recebidos do cliente ->  ", client, ": ", e.args)
+                con.send(bytes('Erro', 'ascii'))
